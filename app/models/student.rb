664cdcdf7eee00ce2529b4c75dc94246c573081a
pedro <pedro.uece.2016@gmail.com>
2019-03-01 13:26:31 -0300

    validação de student e alertas no painel administrativo

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,9 @@
 class Student < ApplicationRecord
   belongs_to :secomp
   has_and_belongs_to_many :events
-  validates_presence_of :name, message: 'é um campo obrigatório !'
-  validates :registration, uniqueness: true, numericality: { only_integer: true, message: "somente números"},
-            length: { is: 7, message: "necessário 7 caracteres" }
   
+  validates_presence_of :name, message: 'é um campo obrigatório !'
+
   validates :email, uniqueness: true, format: { with: /\b[A-Z0-9._%a-z\-]/,
   message: 'é um campo obrigatório !' }
 
