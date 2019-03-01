536b64353e59f680e4febca6d1588630a052c23a
pedro <pedro.uece.2016@gmail.com>
2019-03-01 13:01:32 -0300

    view de marcar presenças

--- app/controllers/backsystem/events_controller.rb
+++ app/controllers/backsystem/events_controller.rb
@@ -29,7 +29,7 @@ class Backsystem::EventsController < BacksystemController
   end
 
   def show
-    if params[:name_search].present? 
+    if params[:name_search].present?
       @students = Student.where(name: params[:name_search]).order(:name)
     elsif params[:email].present?
       @students = Student.where(email: params[:email]).order(:name)
